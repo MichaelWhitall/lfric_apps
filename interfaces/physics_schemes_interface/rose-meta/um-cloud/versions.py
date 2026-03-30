0013cbf6c4d8d5917caddfcefee3be3c10c9c9f3
MichaelWhitall <43407209+MichaelWhitall@users.noreply.github.com>
2026-03-30 17:59:37 +0100

    Revert "Ran the apply_macros.py script."
    This reverts commit 1159d246b9ad02f4150281720307ee4c145c4ec9.

--- interfaces/physics_schemes_interface/rose-meta/um-cloud/versions.py
+++ interfaces/physics_schemes_interface/rose-meta/um-cloud/versions.py
@@ -20,26 +20,29 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
+
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
+
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
+
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
 """
 
-
 class vn31_t243(MacroUpgrade):
-    """Upgrade macro for ticket #243 by Mike Whitall."""
+    # Upgrade macro for Issue #243 by Mike Whitall
 
     BEFORE_TAG = "vn3.1"
     AFTER_TAG = "vn3.1_t243"
 
     def upgrade(self, config, meta_config=None):
-        # Commands From: rose-meta/um-cloud
+        # Add settings
+
         nml = "namelist:cloud"
-        self.add_setting(config, [nml, "l_ensure_max_in_cloud_pc2"], ".false.")
-        self.add_setting(config, [nml, "l_pc2_checks_cfffix"], ".false.")
+        self.add_setting(config, [nml,"l_ensure_max_in_cloud_pc2"], ".false.")
+        self.add_setting(config, [nml,"l_pc2_checks_cfffix"], ".false.")
 
         return config, self.reports
