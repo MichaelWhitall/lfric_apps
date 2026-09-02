a385f6c062393ceae9b1eba0df9236430fd1e99b
MichaelWhitall <43407209+MichaelWhitall@users.noreply.github.com>
2026-09-03 00:14:54 +0100

    Ran the apply_macros script.

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -49,3 +49,86 @@ class vn32_t634(MacroUpgrade):
         self.add_setting(config, [nml, "init_sort_theta"], ".true.")
 
         return config, self.reports
+
+
+class vn32_t717(MacroUpgrade):
+    """Upgrade macro for ticket None by None."""
+
+    BEFORE_TAG = "vn3.2_t634"
+    AFTER_TAG = "vn3.2_t717"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/um-convection
+        # Add new comorph namelist
+        # Append after 'convection' in configuration.nml
+        source = self.get_setting_value(
+            config, ["file:configuration.nml", "source"]
+        )
+        source = re.sub(
+            r"(\(?)namelist:convection(\)?)(\n)",
+            r"\1namelist:convection\2\3 (namelist:comorph)\n",
+            source,
+        )
+        self.change_setting_value(
+            config, ["file:configuration.nml", "source"], source
+        )
+        # Move existing comorph namelist entries from the "convection"
+        # namelist to the new "comorph" namelist.
+        nml1 = "namelist:convection"
+        nml2 = "namelist:comorph"
+        for entry in [
+            "par_gen_mass_fac",
+            "par_gen_rhpert",
+            "par_radius_ppn_max",
+            "resdep_precipramp",
+            "dx_ref",
+        ]:
+            source = self.get_setting_value(config, [nml1, entry])
+            self.remove_setting(config, [nml1, entry])
+            self.add_setting(config, [nml2, entry], source)
+        # Add new namelist entries with previous hardwired default values
+        nml = "namelist:comorph"
+        # Top-level settings
+        self.add_setting(config, [nml, "n_dndraft_types"], "1")
+        self.add_setting(config, [nml, "cf_conv_fac"], "2.0")
+        self.add_setting(config, [nml, "wind_w_buoy_fac"], "1.0")
+        self.add_setting(config, [nml, "overlap_power"], "0.5")
+        self.add_setting(config, [nml, "rain_area_min"], "0.05")
+        # Conv triggering and parcel initialisation
+        self.add_setting(config, [nml, "par_gen_pert_fac"], "0.333")
+        self.add_setting(config, [nml, "par_gen_core_fac"], "3.0")
+        self.add_setting(
+            config, [nml, "par_radius_init_method"], "'linear_p_q'"
+        )
+        self.add_setting(config, [nml, "par_radius_knob"], "0.45")
+        self.add_setting(config, [nml, "par_radius_knob_max"], "2.0")
+        self.add_setting(config, [nml, "ass_min_radius"], "500.0")
+        # Plume model
+        self.add_setting(config, [nml, "ent_coef"], "0.2")
+        self.add_setting(config, [nml, "core_ent_cmr"], ".true.")
+        self.add_setting(config, [nml, "core_ent_fac"], "1.0")
+        self.add_setting(config, [nml, "min_cmr"], "2.0")
+        self.add_setting(config, [nml, "max_cmr"], "6.0")
+        self.add_setting(config, [nml, "drag_coef_par"], "0.5")
+        self.add_setting(
+            config, [nml, "par_radius_evol_method"], "'no_detrain'"
+        )
+        # In-plume microphysics
+        self.add_setting(config, [nml, "autoc_opt"], "'quadratic'")
+        self.add_setting(config, [nml, "coef_auto"], "0.025")
+        self.add_setting(config, [nml, "hetnuc_temp"], "263.0")
+        self.add_setting(config, [nml, "cf_area_coef"], "10.0")
+        self.add_setting(config, [nml, "wind_w_fac"], "1.0")
+        self.add_setting(config, [nml, "col_eff_coef"], "1.0")
+        self.add_setting(config, [nml, "drag_coef_cond"], "0.5")
+        self.add_setting(config, [nml, "vent_factor"], "0.25")
+        self.add_setting(config, [nml, "rho_rim"], "600.0")
+        self.add_setting(config, [nml, "nconc_cl"], "1.0E8")
+        self.add_setting(config, [nml, "nconc_rain"], "1000.0")
+        self.add_setting(config, [nml, "nconc_cf"], "300.0")
+        self.add_setting(config, [nml, "nconc_snow"], "300.0")
+        self.add_setting(config, [nml, "nconc_graup"], "100.0")
+        self.add_setting(config, [nml, "tdep_n_cl"], "0.0")
+        self.add_setting(config, [nml, "tdep_n_cf"], "8.18")
+
+        return config, self.reports
